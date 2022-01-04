de79917cb38c8df878f89d452b9e53092409c7de
Arose114 <akinolakemi70@yahoo.com>
2022-01-04 01:48:23 +0100

    created the models for the contact class

--- Phonebook/phone_contacts/models.py
+++ Phonebook/phone_contacts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Contacts(models.Model):
+    CATEGORY=(
+        ('Family','Family'),
+        ('Friends','Friends'),
+        ('Work','Work'),
+        ('Home','Home'),
+        ('School','School'),
+    )
+    phone_number=models.CharField(null=True, max_length=50)
+    contact_name=models.CharField(null=True, max_length=50)
+    contact_address=models.CharField(null=True, max_length=50)
+    category=models.CharField(null=True, max_length=50, choices=CATEGORY)
+    profile_pic=models.ImageField(null=True, blank=True)
+    date_created =models.DateTimeField(auto_now_add=True, null=True)
+
+
+
+
+    
+
+    def __str__(self):
+        return self.name
+
+  
+
+
